11f6d446479667e8b4d4040fb014306f34b86e88
Alvaro Pompa <alvaropompa@gmail.com>
2018-02-13 21:21:31 +0100

    feat: user experience login

--- irontracking/routes/index.js
+++ irontracking/routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
+// /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  res.render('index', { user: req.user });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { user: req.user });
+// });
 
 
-module.exports = router;
+// module.exports = router;
