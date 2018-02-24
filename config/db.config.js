9c3a44af8467b680061528343e43fad2961c6482
Miguel Manzano <miguelmanzan@gmail.com>
2018-02-24 14:39:30 +0100

    chore: fix structure 2

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
+const DB_NAME = 'it-dev';
+const MONGO_URI = process.env.MONGO_URI || `mongodb://localhost/${DB_NAME}`;
+
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.info(`Connected to database: ${DB_NAME}`)
+  })
+  .catch(error => {
+    console.error(error);
+  });
